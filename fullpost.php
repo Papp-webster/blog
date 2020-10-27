5eec8aadd25c1817c2ccabd1e1c3046aac4e7ba1
Papp László <papp.laszlo.web@gmail.com>
2020-10-27 10:43:26 +0100

    finsih comment design

--- fullpost.php
+++ fullpost.php
@@ -1,7 +1,7 @@
 <?php require_once("includes/db.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 <?php require_once("includes/sessions.php"); ?>
-
+<?php $idFrom = $_GET['id']; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,8 +21,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="index.php">Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarscms"
-          aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarscms" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -54,7 +53,7 @@
 
               <input type="text" placeholder="Search.." name="search">
               <button type="submit" name="searchbutton"><i class="fa fa-search"></i></button>
-           </form>
+            </form>
           </ul>
 
         </div>
@@ -72,53 +71,85 @@
         <h1 class="lead">Design by Papp László</h1>
 
 
-    <?php
-     global $db;
-     if(isset($_GET["searchbutton"])){
-         $search = $_GET["search"];
-         $sql ="SELECT * FROM post WHERE datetime LIKE :search OR post_title LIKE :search OR post_cat LIKE :search OR post_content LIKE :search";
-         $stmt = $db->prepare($sql);
-         $stmt->bindValue(':search', '%'.$search.'%');
-         $stmt->execute();       
-     } else {
-     $id = $_GET["id"];
-     if(!isset($id)){
-       $_SESSION['error'] = "Nincs ilyen poszt!!";
-       redirect("index.php");
-     }
-     $sql = "SELECT * FROM post WHERE post_id='$id' ";
-     $stmt = $db->query($sql);
-     }
-   
-    
-    while($Rows = $stmt->fetch()) {
-     $post_id = $Rows['post_id'];
-     $datetime = $Rows['datetime'];
-     $post_title = $Rows['post_title'];
-     $post_cat = $Rows['post_cat'];
-     $post_author = $Rows['post_author'];
-     $post_img = $Rows['post_img'];
-     $post_content = $Rows['post_content'];
-    
-    
-    
-    
-    ?>
-        <div class="card">
-          <img src="img/<?php echo $post_img; ?>" style="max-height:450px; width:400px;" class="img-fluid card-img-top"
-            alt="pic">
-          <div class="card-body">
-            <h4 class="card-title"><?php echo htmlentities($post_title); ?></h4>
-            <small class="text-muted">Szerző: <?php echo htmlentities($post_author); ?> <i class="fa fa-clock-o"></i>
-              <?php echo htmlentities($datetime); ?></small>
-            <span style="float:right;" class="badge badge-dark text-light">Comments 13</span>
-            <hr>
-            <p class="card-text"><?php echo htmlentities($post_content); ?></p>
+        <?php
+        global $db;
+        if (isset($_GET["searchbutton"])) {
+          $search = $_GET["search"];
+          $sql = "SELECT * FROM post WHERE datetime LIKE :search OR post_title LIKE :search OR post_cat LIKE :search OR post_content LIKE :search";
+          $stmt = $db->prepare($sql);
+          $stmt->bindValue(':search', '%' . $search . '%');
+          $stmt->execute();
+        } else {
+          $id = $_GET["id"];
+          if (!isset($id)) {
+            $_SESSION['error'] = "Nincs ilyen poszt!!";
+            redirect("index.php");
+          }
+          $sql = "SELECT * FROM post WHERE post_id='$id' ";
+          $stmt = $db->query($sql);
+        }
+
+
+        while ($Rows = $stmt->fetch()) {
+          $post_id = $Rows['post_id'];
+          $datetime = $Rows['datetime'];
+          $post_title = $Rows['post_title'];
+          $post_cat = $Rows['post_cat'];
+          $post_author = $Rows['post_author'];
+          $post_img = $Rows['post_img'];
+          $post_content = $Rows['post_content'];
+
+
+
+
+        ?>
+          <div class="card">
+            <img src="img/<?php echo $post_img; ?>" style="max-height:450px; width:400px;" class="img-fluid card-img-top" alt="pic">
+            <div class="card-body">
+              <h4 class="card-title"><?php echo htmlentities($post_title); ?></h4>
+              <small class="text-muted">Szerző: <?php echo htmlentities($post_author); ?> <i class="fa fa-clock-o"></i>
+                <?php echo htmlentities($datetime); ?></small>
+              <span style="float:right;" class="badge badge-dark text-light">Comments 13</span>
+              <hr>
+              <p class="card-text"><?php echo htmlentities($post_content); ?></p>
+            </div>
+          </div>
+        <?php } ?>
+        <!-- comment area -->
+        <div class="comment-area mb-4">
+          <form action="fullpost.php?id=<?php echo $idFrom; ?>" method="post"></form>
+          <div class="card mt-3">
+            <div class="card-header">
+              <h5 class="fieldinfo">Comment section</h5>
+              <div class="card-body">
+                <div class="form-group">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fa fa-user"></i></span>
+                    </div>
+                    <input class="form-control" type="text" name="commentName" placeholder="Name" value="">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="fa fa-envelope"></i></span>
+                    </div>
+                    <input class="form-control" type="email" name="commentEmail" placeholder="Email" value="">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <textarea name="commentWrite" class="form-control" cols="80" rows="6"></textarea>
+                </div>
+                <div>
+                  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <?php } ?>
       </div>
-
+      <!-- Side area -->
       <div class="col-sm-4" style="min-height:40px;">
         <h3>Ez itt a reklám helye!</h3>
       </div>
@@ -127,7 +158,7 @@
 
   </div> <!-- main area end-->
 
-  <!-- Side area -->
+
 
 
 
@@ -144,11 +175,9 @@
   </footer>
   <div class="line">
     <div>
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
       </script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
       </script>
       <script src="js/bootstrap.min.js"></script>
       <script>
--- index.php
+++ index.php
@@ -20,8 +20,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="index.php">Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarscms"
-          aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarscms" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -53,7 +52,7 @@
 
               <input type="text" placeholder="Search.." name="search">
               <button type="submit" name="searchbutton"><i class="fa fa-search"></i></button>
-           </form>
+            </form>
           </ul>
 
         </div>
@@ -69,57 +68,57 @@
       <div class="col-sm-8">
         <h1>This the Blog</h1>
         <h1 class="lead">Design by Papp László</h1>
-     
-             <?php echo message();  echo Successmessage(); ?>
-    <?php
-     global $db;
-     if(isset($_GET["searchbutton"])){
-         $search = $_GET["search"];
-         $sql ="SELECT * FROM post WHERE datetime LIKE :search OR post_title LIKE :search OR post_cat LIKE :search OR post_content LIKE :search";
-         $stmt = $db->prepare($sql);
-         $stmt->bindValue(':search', '%'.$search.'%');
-         $stmt->execute();       
-     } else {
-     $sql = "SELECT * FROM post ORDER BY post_id DESC";
-     $stmt = $db->query($sql);
-     }
-   
-    
-    while($Rows = $stmt->fetch()) {
-     $post_id = $Rows['post_id'];
-     $datetime = $Rows['datetime'];
-     $post_title = $Rows['post_title'];
-     $post_cat = $Rows['post_cat'];
-     $post_author = $Rows['post_author'];
-     $post_img = $Rows['post_img'];
-     $post_content = $Rows['post_content'];
-    
-    
-    
-    
-    ?>
-        <div class="card">
-          <img src="img/<?php echo $post_img; ?>" style="max-height:450px; width:400px;" class="img-fluid card-img-top"
-            alt="pic">
-          <div class="card-body">
-            <h4 class="card-title"><?php echo htmlentities($post_title); ?></h4>
-            <small class="text-muted">Szerző: <?php echo htmlentities($post_author); ?> <i class="fa fa-clock-o"></i>
-              <?php echo htmlentities($datetime); ?></small>
-            <span style="float:right;" class="badge badge-dark text-light">Comments 13</span>
-            <hr>
-            <p class="card-text"><?php 
-        if(strlen($post_content) > 150){
-          $post_content = substr($post_content, 0, 200). "...";
+
+        <?php echo message();
+        echo Successmessage(); ?>
+        <?php
+        global $db;
+        if (isset($_GET["searchbutton"])) {
+          $search = $_GET["search"];
+          $sql = "SELECT * FROM post WHERE datetime LIKE :search OR post_title LIKE :search OR post_cat LIKE :search OR post_content LIKE :search";
+          $stmt = $db->prepare($sql);
+          $stmt->bindValue(':search', '%' . $search . '%');
+          $stmt->execute();
+        } else {
+          $sql = "SELECT * FROM post ORDER BY post_id DESC";
+          $stmt = $db->query($sql);
         }
-        echo htmlentities($post_content); ?></p>
-            <a href="fullpost.php?id=<?php echo $post_id; ?>" style="float:right;">
-              <span class="btn btn-info">Olvass tovább..</span>
-            </a>
+
+
+        while ($Rows = $stmt->fetch()) {
+          $post_id = $Rows['post_id'];
+          $datetime = $Rows['datetime'];
+          $post_title = $Rows['post_title'];
+          $post_cat = $Rows['post_cat'];
+          $post_author = $Rows['post_author'];
+          $post_img = $Rows['post_img'];
+          $post_content = $Rows['post_content'];
+
+
+
+
+        ?>
+          <div class="card">
+            <img src="img/<?php echo $post_img; ?>" style="max-height:450px; width:400px;" class="img-fluid card-img-top" alt="pic">
+            <div class="card-body">
+              <h4 class="card-title"><?php echo htmlentities($post_title); ?></h4>
+              <small class="text-muted">Szerző: <?php echo htmlentities($post_author); ?> <i class="fa fa-clock-o"></i>
+                <?php echo htmlentities($datetime); ?></small>
+              <span style="float:right;" class="badge badge-dark text-light">Comments 13</span>
+              <hr>
+              <p class="card-text"><?php
+                                    if (strlen($post_content) > 150) {
+                                      $post_content = substr($post_content, 0, 200) . "...";
+                                    }
+                                    echo htmlentities($post_content); ?></p>
+              <a href="fullpost.php?id=<?php echo $post_id; ?>" style="float:right;">
+                <span class="btn btn-info">Olvass tovább..</span>
+              </a>
+            </div>
           </div>
-        </div>
         <?php } ?>
       </div>
-
+      <!-- Side area -->
       <div class="col-sm-4" style="min-height:40px;">
         <h3>Ez itt a reklám helye!</h3>
       </div>
@@ -128,7 +127,7 @@
 
   </div> <!-- main area end-->
 
-  <!-- Side area -->
+
 
 
 
@@ -145,11 +144,9 @@
   </footer>
   <div class="line">
     <div>
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
       </script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
       </script>
       <script src="js/bootstrap.min.js"></script>
       <script>
